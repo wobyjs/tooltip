cae70d4b7d8d48a1feb3002de0ca8ed0260adddd
jeehan <tjh@test.com>
2025-09-17 09:57:27 +0800

    merge

---
+++ src/lib/Arrow.tsx
@@ -0,0 +1,10 @@
+import { $$, ObservableMaybe, useMemo, type JSX } from 'woby'
+
+
+export const Arrow = ({ isHovered, hovBkg, bkgCol, flat }: { isHovered: ObservableMaybe<boolean>, hovBkg: ObservableMaybe<string>, bkgCol: ObservableMaybe<string>, flat?: ObservableMaybe<boolean> }): JSX.Element => {
+    const backgroundColor = useMemo(() => $$(isHovered) ? $$(hovBkg) : $$(bkgCol))
+    // const boxShadow = flat ? null : 'shadow-[rgba(0,0,0,.18)0_0_0_1px]'
+    const boxShadow = useMemo(() => $$(flat) ? null : '[box-shadow:rgba(0,0,0,0.18)0px_0px_0px_1px]')
+
+    return <div class={[`rotate-45 w-[14px] h-[14px] m-[3px] z-[1]`, backgroundColor, boxShadow]} />
+}
